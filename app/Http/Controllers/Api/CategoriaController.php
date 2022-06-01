ac48763ee935a3c49ad3add8df0ce48a8beca632
Edmétrio J <edmetrio06@gmail.com>
2022-06-01 16:12:32 +0200

    controller categoria

---
+++ app/Http/Controllers/Api/CategoriaController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Categoria;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Categoria::orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        if($icon = $request->file('icon')){
+            $destino = 'assets/images/categoria';
+            $perfil = date('YmdHis') . "." . $icon->getClientOriginalExtension();
+            $icon->move($destino, $perfil);
+            $input['icon'] = "$perfil";
+        }
+
+        $categoria = Categoria::create($input);
+        if($categoria){
+            return ["resultado"=>"Categoria criada com sucesso"];
+        } else {
+            return ["resultado"=>"Erro ao Adicionar"];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Categoria::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+        $categoria = Categoria::findOrFail($id);
+        if($icon = $request->file('icon')){
+            File::delete(public_path("assets/images/categoria/". $categoria->icon));
+            $destino = 'assets/images/categoria';
+            $perfil = date('YmHis') . "." . $icon->getClientOriginalExtension();
+            $icon->move($destino, $perfil);
+            $input['icon'] = "$perfil";
+        } else {
+            unset($input['icon']);
+        }
+
+        $categoria->update($input);
+        if($categoria){
+            return ["resultado"=>"Categoria Actualizada com sucesso"];
+        } else {
+            return ["resultado"=>"Erro ao Actualizar"];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+
+            File::delete(public_path("assets/images/categoria/".$categoria->icon));
+            $categoria->delete();
+            return ["resultado" => "Apaga com sucesso!"];
+    }
+}
