25536dd4350259a99dd5580f04758d49e3237bd9
Edmétrio J <edmetrio06@gmail.com>
2022-06-14 11:24:12 +0200

    model estoque

--- app/Models/Models/Estoque.php
+++ app/Models/Models/Estoque.php
@@ -2,10 +2,29 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Estoque extends Model
+class Estoque extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'estoque';
+    protected $fillable = ['produto_id','quantidade','pais_id','distrito_id','estado'];
+
+    public function produtos()
+    {
+        return $this->belongsTo(Produto::class, 'produto_id', 'id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'Itemestoque', 'estoque_id', 'usersFor_id');
+    }
 }
