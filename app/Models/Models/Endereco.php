c22bab0e2690d20c20c6406b213ebd22da3c74d0
Edmétrio J <edmetrio06@gmail.com>
2022-06-14 11:24:00 +0200

    model endereco

--- app/Models/Models/Endereco.php
+++ app/Models/Models/Endereco.php
@@ -2,10 +2,29 @@
 
 namespace App\Models\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Endereco extends Model
+class Endereco extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'endereco';
+    protected $fillable = ['users_id','distrito_id','nome','estado'];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function distritos()
+    {
+        return $this->belongsTo(Distrito::class, 'distrito_id', 'id');
+    }
 }
