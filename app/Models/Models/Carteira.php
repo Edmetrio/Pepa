65578c2d396e59233dee75e6e31ae0755ec05242
Edmétrio J <edmetrio06@gmail.com>
2022-06-14 11:23:01 +0200

    model carteira

--- app/Models/Models/Carteira.php
+++ app/Models/Models/Carteira.php
@@ -4,8 +4,21 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Carteira extends Model
+class Carteira extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'carteira';
+    protected $fillable = ['nome','estado'];
+
+    public function enderecos()
+    {
+        return $this->hasMany(Endereco::class, 'id', 'carteira_id');
+    }
 }
