eb1dc9dd97f817cbdc424b0a3a928f1566f8e886
Edmétrio J <edmetrio06@gmail.com>
2022-06-14 11:18:43 +0200

    controller categoria

---
+++ app/Http/Controllers/Api/CarteiraController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Carteira;
+use Illuminate\Http\Request;
+
+class CarteiraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Carteira::with('enderecos')->orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $carteira = Carteira::create($request->all());
+        if($carteira){
+            return ["resultado"=>"Carteira criada com sucesso"];
+        } else {
+            return ["resultado"=>"Erro ao Adicionar"];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Carteira::with('enderecos')->orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Carteira $carteira)
+    {
+        $carteira->update($request->all());
+        return ["resultado"=>"Carteira actualizada com sucesso"];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Carteira $carteira)
+    {
+        $carteira->delete();
+        return ["resultado"=>"Carteira apagada com sucesso"];
+    }
+}
