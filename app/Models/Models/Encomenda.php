770afab9ab4bc3d87a9fed93637e1f692225876f
Edmétrio J <edmetrio06@gmail.com>
2022-06-14 11:23:24 +0200

    model encomenda

--- app/Models/Models/Encomenda.php
+++ app/Models/Models/Encomenda.php
@@ -4,8 +4,21 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Encomenda extends Model
+class Encomenda extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'encomenda';
+    protected $fillable = ['users_id','formapagamento_id','moeda_id','numero','valor','estado'];
+
+    public function produtos()
+    {
+        return $this->belongsToMany(Produto::class, 'itemencomenda', 'encomenda_id', 'produto_id')->withPivot(['quantidade']);
+    }
 }
