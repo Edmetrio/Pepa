6c861b688071d75592312d517d367eac65898004
Edmétrio J <edmetrio06@gmail.com>
2022-06-15 16:22:05 +0200

    model produtos

---
+++ app/Models/Models/Produto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+
+class Produto extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'produto';
+    protected $fillable = ['categoria_id','nome','icon','descricao','preco_retalho','preco_grosso','estado'];
+
+    public function categorias()
+    {
+        return $this->hasOne(Categoria::class, 'id', 'categoria_id');
+    }
+}
