de8f6df713461947c32c4fd6d1a3383bee6d87ea
Edmétrio J <edmetrio06@gmail.com>
2022-06-24 09:33:36 +0200

    model forma pagamento

--- app/Models/Models/Formapagamento.php
+++ app/Models/Models/Formapagamento.php
@@ -4,8 +4,16 @@ namespace App\Models\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
 
-class \Formapagamento extends Model
+class Formapagamento extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'formapagamento';
+    protected $fillable = ['nome','estado'];
 }
