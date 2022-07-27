c69dbea95c3e8daf58c1e4aa0b1b13a22a3dac7b
Edmétrio J <edmetrio06@gmail.com>
2022-07-27 12:10:27 +0200

    Distrito controller

---
+++ app/Http/Controllers/Api/DistritoController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Distrito;
+use App\Models\Models\Estoque;
+use Illuminate\Http\Request;
+
+class DistritoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Distrito::with('provincias','produtos')->orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'distrito_id' => 'required',
+            'produto_id' => 'required'
+        ]);
+
+        $estoque = Estoque::where('produto_id', $request->produto_id)
+        ->where('distrito_id', $request->distrito_id)->first();
+        return $estoque;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+        return Distrito::with('provincias','produtos')->where('provincia_id', $id)->orderBy('created_at', 'desc')->get();
+    }
+
+    public function getproduto()
+    {
+
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
