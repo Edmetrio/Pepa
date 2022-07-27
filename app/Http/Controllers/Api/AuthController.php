65feb6d4d5e789bcd223a5333950a4fe963eb92f
Edmétrio J <edmetrio06@gmail.com>
2022-07-27 12:10:16 +0200

    Auth controller api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Models\Extensionistafornecedor;
+use App\Models\Models\Telefone;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\RegisterMail;
 
 class AuthController extends Controller
 {
@@ -13,6 +17,8 @@ class AuthController extends Controller
     {
         $request->validate([
             'name' => 'required|string',
+            'role_id' => ['required'],
+            'numero' => ['required'],
             'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed'
         ]);
@@ -21,10 +27,30 @@ class AuthController extends Controller
             'role_id' => $request->role_id,
             'name' => $request->name,
             'email' => $request->email,
+            'numero' => $request->numero,
+            'role_id' => $request->role_id,
             'password' => bcrypt($request->password)
         ]);
 
         $token = $user->createToken('token')->plainTextToken;
+
+        if($request->role_id === '9t243f2e-ef1e-4454-9ae2-34d091efbc8p')
+        {
+            Extensionistafornecedor::create([
+                'extensionista_id' => 'h962bb71-3ccf-4994-9c60-0dba5f49a7y8',
+                'users_id' => $user->id,
+                'situacao_id' => '3gd66d9f-614f-8adc-994f-a578099e95j8'
+            ]);
+            $details = $request->all();
+            Mail::to('admin@firsteducation.edu.mz')->send(new RegisterMail($details));
+            Mail::to('extensionista@firsteducation.edu.mz')->send(new RegisterMail($details));
+        } else {
+
+        }
+
+        $input = $request->all();
+        $input['users_id'] = $user->id;
+        Telefone::create($input);
 
         $response = [
             'user' => $user,
@@ -33,6 +59,7 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    
 
     public function login(Request $request)
     {
