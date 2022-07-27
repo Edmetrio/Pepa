676747352b0cbb23f4e469fb7960f89fd40a5774
Edmétrio J <edmetrio06@gmail.com>
2022-07-27 12:11:27 +0200

    Role controller

---
+++ app/Http/Controllers/Api/RoleController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Role::with('rotas')->where('nome', 'cliente')->orWhere('nome', 'fornecedor')->orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+        ]);
+
+        $role = Role::create($request->all());
+        if($role){
+            return ["resultado"=>"Role criado com sucesso"];
+        } else {
+            return ["resultado"=>"Erro ao Adicionar"];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Role::with('rotas')->findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $request->validate([
+            'nome' => 'required',
+        ]);
+
+        $role->update($request->all());
+        return ["resultado"=>"Role actualizado com sucesso"];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+        return ["resultado"=>"Role deletado com sucesso"];
+    }
+}
