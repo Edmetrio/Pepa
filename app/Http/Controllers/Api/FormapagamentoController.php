7a05c4659e44c9382c9295c77f4a5547a4d0b3ea
Edmétrio J <edmetrio06@gmail.com>
2022-07-28 15:32:39 +0200

    resource formapagamento

---
+++ app/Http/Controllers/Api/FormapagamentoController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Formapagamento;
+use Illuminate\Http\Request;
+
+class FormapagamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Formapagamento::orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $pagamento = Formapagamento::create($request->all());
+        if($pagamento){
+            return ["resultado"=>"Forma de pagamento criado com sucesso"];
+        } else {
+            return ["resultado"=>"Erro ao Adicionar"];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Formapagamento::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $pagamento = Formapagamento::findOrFail($id)->update($request->all());
+        if($pagamento){
+            return ["resultado"=>"Forma de pagamento Actualizado com sucesso"];
+        } else {
+            return ["resultado"=>"Erro ao Actualizar"];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Formapagamento $pagamento)
+    {
+        $pagamento->delete();
+        return ["resultado"=>"Deletado com sucesso"];
+    }
+}
