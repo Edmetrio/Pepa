b83bb9339e15db9d46886cdd30cf1176b0ee0162
Edmétrio J <edmetrio06@gmail.com>
2022-07-28 15:34:09 +0200

    paginate

--- app/Http/Livewire/Inicios.php
+++ app/Http/Livewire/Inicios.php
@@ -7,10 +7,11 @@ use App\Models\Models\Itemcarrinha;
 use App\Models\Models\Produto;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Inicios extends Component
 {
-
+    use WithPagination;
     public function render()
     {
         $item = Itemcarrinha::with('produtos', 'users', 'distritos', 'enderecos', 'unidades')->where(function ($query){
@@ -18,15 +19,34 @@ class Inicios extends Component
                 $query->where('users_id', Auth::user()->id);
             }
         })->get();
+
         $tt = 0.0;
         foreach ($item as $itens) {
             $itens->subtotal = $itens->quantidade * $itens->produtos->preco_retalho;
             $tt += $itens->subtotal;
         }
         $item->total = $tt;
-        $produto = Produto::orderBy('created_at', 'desc')->get();
+        $produto = Produto::orderBy('created_at', 'asc')->paginate(4);
+        $produtos = Produto::orderBy('created_at', 'desc')->paginate(8);
+        /* $this->produtos = Produto::orderBy('created_at', 'desc')->get(); */
         $categoria = Categoria::orderBy('created_at', 'desc')->get();
-        return view('livewire.inicios', compact('categoria','produto'))->layout('layouts.app', compact('produto','categoria','item'));
+        return view('livewire.inicios', compact('categoria','produto','produtos'))->layout('layouts.app', compact('produto','categoria','item'));
+    }
+
+    public function delete($id)
+    {
+        $item = Itemcarrinha::findOrFail($id);
+        $item->delete();
+        $this->alertSuccess();
+    }
+
+    public function alertSuccess()
+    {
+        $this->dispatchBrowserEvent('swal:modal', [
+            'type' => 'success',
+            'message' => 'Item apagado!',
+            'text' => 'O Item foi apagado com sucesso.'
+        ]);
     }
 
 }
