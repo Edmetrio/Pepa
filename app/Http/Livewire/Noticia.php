c8309acce57248f2fbcacf64c9ac82b34e4903f9
Edmétrio J <edmetrio06@gmail.com>
2022-07-28 15:34:51 +0200

    livewire noticias

---
+++ app/Http/Livewire/Noticia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Noticia extends Component
+{
+    public function render()
+    {
+        return view('livewire.noticia');
+    }
+}
